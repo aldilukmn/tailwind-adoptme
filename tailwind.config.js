a022a2ff6565770bad989c989d673ba3113b3d21
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-04-14 23:56:07 +0700

    navbar adoptme

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sg: ["Sawarabi Gothic", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
